50601c6e339e61174d618d5efd50370c634d0531
winwu <look_me08@hotmail.com>
2024-07-14 17:07:37 -0700

    update shared/component customButton style

--- shared/components/src/CustomButton.tsx
+++ shared/components/src/CustomButton.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./CustomButton.css";
 
 const CustomButton = (props: React.PropsWithChildren<{ id: string; onClick: React.MouseEventHandler<HTMLButtonElement> }>) => {
 
     return (
-        <button id={props.id} onClick={props.onClick}>{props.children}</button>
+        <button id={props.id} className="s-btn" onClick={props.onClick}>{props.children}</button>
     )
 }
 export default CustomButton;
