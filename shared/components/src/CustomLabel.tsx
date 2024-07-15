d5918ca2c67aa30944d63b4192e4ba1692cccf74
winwu <look_me08@hotmail.com>
2024-07-14 17:10:10 -0700

    update shared/component customLabel style

--- shared/components/src/CustomLabel.tsx
+++ shared/components/src/CustomLabel.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import "./CustomLabel.css";
 
 const CustomLabel = (props: React.PropsWithChildren<{htmlFor?: string}>) => {
     return (
-        <label {...(props?.htmlFor ? {htmlFor: props.htmlFor} : {})}>{props.children}</label>
+        <label {...(props?.htmlFor ? {htmlFor: props.htmlFor} : {})} className='s-label'>{props.children}</label>
     )
 }
 
